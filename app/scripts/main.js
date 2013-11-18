586e2cc51b39be81b173eae77eb5316bacca4e4a
Tyler Phillips <tylerfphillips@gmail.com>
2013-11-18 10:48:54 -0500

    Hour 1-2

---
+++ app/scripts/main.js
@@ -0,0 +1,65 @@
+// HOUR 1
+
+// Alert Pop-up
+	// window.alert("Here is my message")
+		// Alert that displays title of page
+		// alert(document.title) 
+
+
+// HOUR 2
+
+// Write characters directly into HTML (not really used much)
+	// document.write("Here is my message")
+
+// Variables
+	var netPrice;
+	netPrice = 8.99;
+
+	var productName = "Leather wallet";
+	// alert(productName)
+
+// Operators
+	var productCount = 2;
+	var subtotal = 14.98;
+	var shipping = 2.75;
+	var total = subtotal + shipping;
+	var salesTax = total - shipping;
+
+	var itemsPerBox = 12;
+	var itemsToBeBoxed = 40;
+	var itemsInLastBox = itemsToBeBoxed % itemsPerBox;
+
+	// Increment Operator
+	  // productCount++;
+	    // equivalent to productCount = productCount + 1;
+	  // Similarly productCount--;
+
+	// total += 5 is the same as total = total + 5
+
+
+// Using the + operator with Strings
+	var firstname = "John";
+	var surname = "Doe";
+	var fullname = firstname + " " + surname;
+	  // the variable fullname now contains the value "John Doe"
+
+	// var name = "David";
+	// var age = 45;
+	// alert(name + age);
+	  // David45 appears in alert box
+
+// Capturing Mouse Events
+  // onClick Event Handler
+  // onMouseOver Event Handler
+  // onMouseOut Event Handler
+
+// HOUR 3
+
+// USING FUNCTIONS
+
+// General Syntax
+  function sayHello() {
+  	alert("Hello")
+	// ... more statements can go here ...
+  }
+
